8cd4945d9da541006e3a16e723dbbd774fcae7d8
David Galichet <david.galichet@gmail.com>
2011-11-20 11:32:55 +0100

    foo, bar and qix never change -> replace def with val

--- src/main/scala/FooBarQix.scala
+++ src/main/scala/FooBarQix.scala
@@ -13,11 +13,11 @@ object FooBarQix {
       }
   }
 
-  def foo = scheme(3, "foo")
-  def bar = scheme(5, "bar")
-  def qix = scheme(7, "qix")
+  val foo = scheme(3, "foo")
+  val bar = scheme(5, "bar")
+  val qix = scheme(7, "qix")
 
-  def fooBarQix = { x:Int => { y:String => (if (y.isEmpty) x.toString else y)}.apply(foo(x) + bar(x) + qix(x)) }
+  val fooBarQix = { x:Int => { y:String => (if (y.isEmpty) x.toString else y)}.apply(foo(x) + bar(x) + qix(x)) }
 
   def main(args: Array[String]) {
     println((1 to 100).map(fooBarQix).mkString("\n"))
