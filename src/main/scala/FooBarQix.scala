4dfe008ad56c17d6a8bee9c57eae458aa6478f08
David Galichet <david.galichet@gmail.com>
2012-02-29 23:09:38 +0100

    Enhance with comments and explicit return types.

--- src/main/scala/FooBarQix.scala
+++ src/main/scala/FooBarQix.scala
@@ -4,8 +4,9 @@
  */
 
 object FooBarQix {
-  
-  def scheme(mod:Int, replacement:String) = { x:Int =>
+
+  // Returns a function that transform an Int to a String
+  def scheme(mod:Int, replacement:String): Int => String = { x:Int =>
     (if (x % mod == 0) replacement else "")  +
       x.toString.fold("") {
         case (acc, elem) if (elem == mod.toString.head) => replacement + acc
@@ -13,11 +14,13 @@ object FooBarQix {
       }
   }
 
+  // Create the three transformations patterns
   val foo = scheme(3, "foo")
   val bar = scheme(5, "bar")
   val qix = scheme(7, "qix")
 
-  val fooBarQix = { x:Int => { y:String => (if (y.isEmpty) x.toString else y)}.apply(foo(x) + bar(x) + qix(x)) }
+  // apply transformations to x and returns the result or x if empty
+  val fooBarQix: Int => String = { x:Int => { y:String => (if (y.isEmpty) x.toString else y)}.apply(foo(x) + bar(x) + qix(x)) }
 
   def main(args: Array[String]) {
     println((1 to 100).map(fooBarQix).mkString("\n"))
